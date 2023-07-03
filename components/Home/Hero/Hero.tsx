c3fb656ec9ff067c81c1493ab841cfa7b707cb3c
tamerhayek <dev@tamerhayek.com>
2023-07-03 18:48:02 +0200

    development: little fix to Headings

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -8,12 +8,12 @@ const Hero = () => {
 				<LottieImage animationData={development} />
 			</div>
 			<div className="py-3 lg:p-10">
-				<h2 className="text-4xl py-2 text-teal-300 font-bold lg:text-8xl">
+				<h1 className="text-4xl py-2 text-teal-300 font-bold lg:text-6xl">
 					I'm Tamer Hayek
-				</h2>
-				<h3 className="text-2xl py-2 md:text-3xl">
+				</h1>
+				<h2 className="text-2xl py-2 md:text-3xl">
 					Full Stack Developer
-				</h3>
+				</h2>
 				<p className="text-md py-2 leading-8 text-gray-400 md:text-xl max-w-xl">
 					I&apos;m a full-stack web developer with a passion for
 					technology and I am dedicated to creating innovative and
