613927afa8edd9f9fca1e4b41da6edb0d11b4de7
tamerhayek <dev@tamerhayek.com>
2023-07-04 14:39:30 +0200

    development: added contact icons in the footer

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import Link from "next/link";
+import { MdEmail } from "react-icons/md";
+import { SiTelegram, SiDiscord } from "react-icons/si";
 
 const Footer = () => {
 	return (
 		<section id="footer" className="my-10 py-10">
-			<div className="flex flex-col items-center justify-center gap-5 min-h-[100px]">
+			<div className="flex flex-col items-center justify-center gap-10 min-h-[100px]">
 				<h3 className="text-gray-400 ">
 					©2023 Tamer Hayek. All rights reserved.
 				</h3>
+				<div className="flex gap-5 pb-5">
+					<Link href={"mailto:dev@tamerhayek.com"}>
+						<MdEmail className="w-7 h-7" />
+					</Link>
+					<Link href={"https://t.me/tamibyte"}>
+						<SiTelegram className="w-7 h-7" />
+					</Link>
+					<Link href={"https://discord.com/channels/tamibyte"}>
+						<SiDiscord className="w-7 h-7" />
+					</Link>
+				</div>
 			</div>
 		</section>
 	);
