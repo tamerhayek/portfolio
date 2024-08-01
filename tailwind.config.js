d722ba5ed3404f1d73726af55069493e6421835a
tamerhayek <dev@tamerhayek.com>
2024-08-01 19:28:07 +0200

    refactor: remove unused daisyui light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,25 +12,6 @@ export default {
 	daisyui: {
 		themes: [
 			{
-				light: {
-					primary: '#6558FF',
-					'primary-focus': '#6531FF',
-					'primary-content': '#ebecf0',
-					secondary: '#5EEAD4',
-					'secondary-focus': '#00cfbd',
-					'secondary-content': '#212121',
-					accent: '#1F3A8A',
-					'accent-focus': '#1F5A8A',
-					'accent-content': '#ebecf0',
-					'base-100': '#020817',
-					'base-200': '#021947',
-					'base-300': '#1C143A',
-					'base-content': '#EBEBF2',
-					success: '#45b56e',
-					error: '#96291e'
-				}
-			},
-			{
 				dark: {
 					primary: '#6558FF',
 					'primary-focus': '#6531FF',
