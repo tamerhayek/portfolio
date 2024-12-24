a1dfecba91a8470790086cd9c4ff2245b07acdaa
tamerhayek <dev@tamerhayek.com>
2024-12-24 18:58:35 +0100

    feat: remove tests, update packages and update node version. Remove server port in vite config

--- tests/test.ts
+++
@@ -1,5 +0,0 @@
-import { test } from '@playwright/test';
-
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
@@ -11,11 +11,5 @@ export default defineConfig({
 		}),
 		sveltekit(),
 		enhancedImages()
-	],
-	server: {
-		port: 3000
-	},
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	]
 });
