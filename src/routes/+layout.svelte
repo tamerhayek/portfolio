745a87d18d8c933b73f7d33355fbf818d5d845e4
tamerhayek <dev@tamerhayek.com>
2025-03-19 15:10:12 +0100

    feat: add umami integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { PUBLIC_UMAMI_WEBSITE_ID } from '$env/static/public';
 	import { i18n } from '$lib/i18n';
 	import { languageTag } from '$lib/paraglide/runtime';
 	import '@fontsource/ubuntu';
 	import '@fontsource/ubuntu/300.css';
+	import '@fontsource/ubuntu/400-italic.css';
+	import '@fontsource/ubuntu/700.css';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	import '../app.css'; // Light
-	import '@fontsource/ubuntu/700.css'; // Bold
-	import '@fontsource/ubuntu/400-italic.css'; // Regular Italic
+	import '../app.css';
 	import Footer from './Footer.svelte';
 	import Navbar from './Navbar.svelte';
 
@@ -30,6 +31,16 @@
 			? 'Sviluppatore web, Fullstack Developer, Tecnologia, Digitale, Sito web'
 			: 'Web developer, Fullstack Developer, Technology, Digital, Website'}
 	/>
+
+	<!-- Umami -->
+	{#if PUBLIC_UMAMI_WEBSITE_ID}
+		<script
+			defer
+			src="https://analytics.olymposhub.io/script.js"
+			data-website-id={PUBLIC_UMAMI_WEBSITE_ID}
+		></script>
+	{/if}
+	<!-- End Umami -->
 </svelte:head>
 
 <ParaglideJS {i18n}>
